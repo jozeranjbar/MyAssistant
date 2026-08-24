00369ba007e6ec75e830a17a4dbdefb1070cedba
جُزرنجبر <jozeranjbar@gmail.com>
2026-08-24 09:35:07 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -71,6 +71,7 @@ class MyAssistantApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.lightBlue,
+        scaffoldBackgroundColor: Colors.lightBlue.shade50,
       ),
       builder: (context, child) {
         // اجرای کل برنامه به‌صورت راست‌به‌چپ (فارسی) و مدیریت ابعاد/چرخش صفحه
