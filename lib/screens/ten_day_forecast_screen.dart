97b02917865e54b470f088e13dee9c715e656d5a
جُزرنجبر <jozeranjbar@gmail.com>
2026-09-01 16:01:44 +0330

    Update ten_day_forecast_screen.dart

--- lib/screens/ten_day_forecast_screen.dart
+++ lib/screens/ten_day_forecast_screen.dart
@@ -28,29 +28,6 @@ class TenDayForecastScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-<<<<<<< HEAD
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.arrow_back_ios, size: 16, color: Colors.brown.shade400),
-            const SizedBox(width: 4),
-            Flexible(
-              child: Text(
-                'وضعیت هوای ده روز آینده ${location.name}',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.brown.shade700,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(width: 4),
-            Icon(Icons.arrow_forward_ios, size: 16, color: Colors.brown.shade400),
-          ],
-=======
-        backgroundColor: Colors.lightBlue.shade100,
         elevation: 0,
         automaticallyImplyLeading: false,
         leading: IconButton(
@@ -69,7 +46,6 @@ class TenDayForecastScreen extends StatelessWidget {
               fontSize: 18,
             ),
           ),
->>>>>>> 175121676c68fb2b22cfa1d383a7e316706316d5
         ),
       ),
       body: GestureDetector(
